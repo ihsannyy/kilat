c8f01b11b950cbb3fe2ef5be9b92700eb3e1ee48
Shannyiee <najihko1@gmail.com>
2026-09-12 12:19:03 +0700

    remove leftover demo-app folder

--- demo-app/index.js
+++
@@ -1,8 +0,0 @@
-// 🚀 Kilat Project: demo-app
-console.log("Hello from Kilat!");
-
-const fs = require('fs');
-const os = require('os');
-
-console.log("OS:", os.getenv("OSTYPE") || "unknown");
-console.log("Files:", fs.readdirSync("."));
