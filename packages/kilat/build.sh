39d4e149073883c176c5c5edf4525203cb9d242a
Shannyiee <najihko1@gmail.com>
2026-09-18 09:14:11 +0700

    chore: add Termux package script

---
+++ packages/kilat/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ihsannyy/kilat
+TERMUX_PKG_DESCRIPTION="Fast JavaScript runtime for Termux"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=5.0.0
+TERMUX_PKG_SRCURL=https://github.com/ihsannyy/kilat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=SKIP_CHECK
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	CGO_ENABLED=0 go build -o kilat ./cmd/kilat
+}
+
+termux_step_make_install() {
+	install -Dm755 kilat "$TERMUX_PREFIX/bin/kilat"
+}
